689217cc79351576602b235b3f32075f42a5e4d7
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-09-30 13:49:39 -0300

    Criando classe de Dominio SocialMetaTag

---
+++ src/main/java/com/ricardo/demoajax/domain/SocialMateTag.java
@@ -0,0 +1,42 @@
+package com.ricardo.demoajax.domain;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class SocialMateTag implements Serializable {
+
+	
+	private String site;
+	private String title;
+	private String url;
+	private String image;
+	
+	
+	
+	public String getSite() {
+		return site;
+	}
+	public void setSite(String site) {
+		this.site = site;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+}
