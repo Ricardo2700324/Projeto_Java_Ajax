db1bd29b895c441d24b2d23791b826b972c441ea
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-10-22 10:35:55 -0300

    Criando camada de persistencia

---
+++ src/main/java/com/ricardo/demoajax/repository/PromocaoRepository.java
@@ -0,0 +1,9 @@
+package com.ricardo.demoajax.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricardo.demoajax.domain.Promocao;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+}
