e75328ba924a6f7933d90144c497d7f0752e729b
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-10-22 11:07:56 -0300

    Funcao AJAX para submeter o formulario

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -1,3 +1,38 @@
+//submit do formulário para o controller
+$("#form-add-promo").submit(function(evt){
+	//bloquear o comportamento padrão do submit
+	evt.preventDefault();
+	
+	
+	var promo = {};
+	promo.linkPromocao = $("#linkPromocao").val();
+	promo.descricao = $("#descricao").val();
+	promo.preco = $("#preco").val();
+	promo.titulo = $("#titulo").val();
+	promo.categoria = $("#categoria").val();
+	promo.linkImagem = $("#linkImagem").attr("src");
+	promo.site = $("#site").text();
+	
+	
+	console.log('promo >', promo);
+	
+	
+	$.ajax({
+		method: "POST",
+		url: "/promocao/save",
+		data: promo, 
+		success: function(){
+			$("alert").addClass("alert alert-success").text("OK! Promoção cadastrada com sucesso!")
+		},
+		error:function(xhr){
+			console.log("> error", xhr.responseText);
+			$("#alert").addClass("alert alert-danger").text("Não foi possivel salvar esta promoção!")
+		}
+	})
+});
+
+
+
 // função para capturar as meta tags
 $('#linkPromocao').on('change', function(){
 	
