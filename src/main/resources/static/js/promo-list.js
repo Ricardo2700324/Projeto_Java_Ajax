19452964341e784503f0dbc462a8235b77c6e7c1
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-10-24 09:56:24 -0300

    Criando o Infinite Scroll

---
+++ src/main/resources/static/js/promo-list.js
@@ -0,0 +1,15 @@
+
+//Efeito Infinite Scroll
+$(window).scroll(function(){
+	
+	var scrollTop = $(this).scrollTop();
+	var conteudo = $(document).height() - $(window).height();
+	
+	
+	console.log('scrollTop: ', scrollTop, ' | ', ' conteudo: ', conteudo);
+	
+	if (scrollTop >= conteudo){
+		console.log("***");
+	}
+	
+});
