deebb0baa5cdb77979685dae0218ff4ba53e7647
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-10-24 10:53:28 -0300

    Infinite Scroll em acao e pronto

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,5 +1,12 @@
 var pageNumber = 0;
 
+//Rotina que trabalha toda vez que a pagina for aberta pela 1º vez
+$(document).ready(function(){
+	$("#loader-img").hide();
+	$("#fim-btn").hide();
+});
+
+
 //Efeito Infinite Scroll
 $(window).scroll(function(){
 	
@@ -7,7 +14,7 @@ $(window).scroll(function(){
 	var conteudo = $(document).height() - $(window).height();
 	
 	
-	console.log('scrollTop: ', scrollTop, ' | ', ' conteudo: ', conteudo);
+	//console.log('scrollTop: ', scrollTop, ' | ', ' conteudo: ', conteudo);
 	
 	if (scrollTop >= conteudo){
 		pageNumber++;
@@ -27,8 +34,29 @@ function loadByScrollBar(pageNumber){
 		data: {
 			page: pageNumber
 		},
+		beforSend: function(){
+			$("#loader-img").show();
+		},
 		success: function( response ){
-			console.log("resposta: ", response)
+			//console.log("resposta: ", response);
+			console.log("lista: ", response.length);
+			
+			if(response.length > 150){
+			
+				$(".row").fadeIn(250, function(){
+					$(this).append(response);
+				});
+				
+			}else{
+				$("#fim-btn").show();
+				$("#loader-img").removeClass("loader");
+			}
+		},
+		error: function(xhr){
+			alert("Ocorreu um erro! " + xhr.status + " - " + xhr.statusText);
+		},
+		complete: function(){
+			$("#loader-img").hide();
 		}
 	})
 }
