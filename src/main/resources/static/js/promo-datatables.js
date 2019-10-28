f3cd85eeadbf143cecd97cb7db2b3fff68748635
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-10-28 16:10:45 -0300

    Preparando o lado cliente(DataTable)

---
+++ src/main/resources/static/js/promo-datatables.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	
+	$("#table-server").DataTable({
+		processing: true,
+		serverSide: true, 
+		responsive: true,
+		lengthMenu: [ 10, 15, 20, 25],
+		ajax:{
+			url: "/promocao/datatables/server",
+			data: "data"
+		},
+		columns: [
+			 {data: 'id'},
+			 {data: 'titulo'},
+			 {data: 'site'},
+			 {data: 'linkPromocao'},
+			 {data: 'descricao'},
+			 {data: 'linkImagem'},
+			 {data: 'preco'},
+			 {data: 'likes'},
+			 {data: 'dtCadastro'},
+			 {data: 'categoria.titulo'}
+		]
+	});
+	
+});
